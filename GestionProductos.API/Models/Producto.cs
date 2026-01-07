fc6bbd9d165a92c8cfed2a57e8ee0e2b10ff11a2
camacho-fran <u00032982@sise.com.pe>
2026-01-06 21:32:32 -0500

    Add Producto model

---
+++ GestionProductos.API/Models/Producto.cs
@@ -0,0 +1,11 @@
+﻿namespace GestionProductos.API.Models
+{
+    public class Producto
+    {
+        public int IdProducto { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+        public bool Estado { get; set; }
+    }
+}
