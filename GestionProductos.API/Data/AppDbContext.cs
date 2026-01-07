6fcf232b2a7e2cb6052f543132c767535f13582d
camacho-fran <u00032982@sise.com.pe>
2026-01-06 21:51:47 -0500

    Update Precio decimal precision using EF Core migration

---
+++ GestionProductos.API/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GestionProductos.API.Models;
+
+namespace GestionProductos.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Producto>()
+                .Property(p => p.Precio)
+                .HasPrecision(18, 2);
+        }
+    }
+}
--- GestionProductos.API/Models/Producto.cs
+++ GestionProductos.API/Models/Producto.cs
@@ -1,8 +1,12 @@
-﻿namespace GestionProductos.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestionProductos.API.Models
 {
     public class Producto
     {
+        [Key]
         public int IdProducto { get; set; }
+
         public string Nombre { get; set; }
         public decimal Precio { get; set; }
         public int Stock { get; set; }
